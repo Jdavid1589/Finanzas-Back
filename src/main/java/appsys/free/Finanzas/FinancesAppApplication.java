10e18b0d910efe5efcb7c636eec704795d583205
Juan David <jdposadas1985@gmail.com>
2025-09-29 23:15:28 -0500

    Render

--- src/main/java/appsys/free/Finanzas/FinancesAppApplication.java
+++ src/main/java/appsys/free/Finanzas/FinancesAppApplication.java
@@ -1,29 +1,17 @@
 package appsys.free.Finanzas;
 
-/*import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;*/
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;*/
 
 @SpringBootApplication
 
 public class FinancesAppApplication  {
-/*public class FinancesAppApplication implements CommandLineRunner {*/
+
 
     public static void main(String[] args) {
         SpringApplication.run(FinancesAppApplication.class, args);
     }
 
-    /*   @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
-
- //Metodo adicional prueba para generar passwEncript
-    @Override
-    public void run(String... args) throws Exception {
-        String passw = passwordEncoder.encode("cap.+45L**");
-        System.out.println("pass: " + passw);
-
-    }*/
 
 }
